baa4a3161e18e4e6b6b71caec8eba9bd36c9db3d
Jorge_dany <spumasc11@gmail.com>
2023-02-04 20:32:12 -0600

    Arbol, scrips vacios y mas

---
+++ Assets/Script/EnemyController.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public int vidas;
+    public int speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
