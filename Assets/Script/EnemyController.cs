b3c1863c2e89546489724894d5aee63b172e2154
Jorge_dany <spumasc11@gmail.com>
2023-02-04 21:00:32 -0600

    VersionNoestable

--- Assets/Script/EnemyController.cs
+++ Assets/Script/EnemyController.cs
@@ -6,10 +6,24 @@ public class EnemyController : MonoBehaviour
 {
     public int vidas;
     public int speed;
+    public GameObject targets;
+    float xpos, ypos;
+    float distance;
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    private void EdoAttack()
+    {
+        Transform pos = GetDestination();
+        
+        xpos = Mathf.Clamp();
+    }
+    private Transform GetDestination()
+    {
+        int lentarg = targets.transform.childCount;
+        return targets.transform.GetChild(Random.Range(0,lentarg));
     }
 }
