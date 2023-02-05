0453ca946a43011d90712427dc61bb9083af6bb1
Quesesho <heazodev@gmail.com>
2023-02-05 13:33:41 -0600

    Destruccion, muerte, cumbia

--- Assets/BulletS.cs
+++ Assets/BulletS.cs
@@ -23,4 +23,12 @@ public class BulletS : MonoBehaviour
     {
         
     }
+
+     private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Enemy"))
+        {
+            Destroy(gameObject);
+        }
+    }
 }
