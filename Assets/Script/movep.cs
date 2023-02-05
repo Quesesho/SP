6fc158687140c8bd38cb9f8b847fb153a18fc14b
Quesesho <heazodev@gmail.com>
2023-02-04 19:32:32 -0600

    xD
    revivio la comedia cabrones

--- Assets/Script/movep.cs
+++ Assets/Script/movep.cs
@@ -11,6 +11,7 @@ public class movep : MonoBehaviour
     private PlayerA playerA;
     public Rigidbody2D rbody;
     private Vector2 moveI;
+    private bool canChange= false;
 
     void Awake() 
     {
@@ -29,17 +30,34 @@ public class movep : MonoBehaviour
 
     private void Attack()
     {
-        Debug.Log("Atacku the Otaku");
+        if(canChange)
+            Debug.Log("Atacku the Otaku");
+        else
+            Debug.Log("Tiradle piedras al otaku");
     }
 
     // Update is called once per frame
     void Update()
     {
         moveI = playerA.PlayerM.Move.ReadValue<Vector2>();
-        if(PlayerManager.instantiate.isArriba){
+        if(!canChange){
             moveI.y=0;
         }
         rbody.velocity = moveI * speed;
         if(playerA.PlayerM.Fire.triggered) Attack();
+    }
+
+    private void OnTriggerStay2D(Collider2D other) {
+        if(other.CompareTag("Tuneles")){
+            canChange = true;
+            Debug.Log("Adentro");
+        }
+
+    }
+
+    private void OnTriggerExit2D(Collider2D other) {
+        if(other.CompareTag("Tuneles")){
+            canChange = false;
+        }
     }
 }
