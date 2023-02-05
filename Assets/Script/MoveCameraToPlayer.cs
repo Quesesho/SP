8f08255b3e5e27cf226639d8ab52d8ce70e0dcb9
Jorge_dany <spumasc11@gmail.com>
2023-02-04 19:42:00 -0600

    Merge branch 'aaaa' into JD

---
+++ Assets/Script/MoveCameraToPlayer.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCameraToPlayer : MonoBehaviour
+{
+    Camera cam;
+    float tmpx, tmpy, tmpz;
+    void Start()
+    {
+        cam = GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        tmpx = PlayerManager.instantiate.transform.position.x;
+        tmpy = PlayerManager.instantiate.transform.position.y;
+        tmpz = cam.transform.position.z;
+        //if (tmpx > -1.2 && tmpx < 1.2)
+        //{
+        cam.transform.position = new Vector3(tmpx, tmpy, tmpz);
+        //}
+
+
+
+    }
+}
